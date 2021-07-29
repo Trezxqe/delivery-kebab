7c055ef9eb021920c3fce985c421a9c2bf7f1fb4
Dmitry Rum <acopalypse@yandex.ru>
2021-07-29 13:37:43 +0300

    middleware fix

--- middlewares/checkAutorize.js
+++ middlewares/checkAutorize.js
@@ -1,5 +1,6 @@
 const checkAutorize = (req, res, next) => {
-  if(req.session.isAutorize && req.session.userId) {
+  if (req.session.isAutorize && req.session.userId) {
+    res.locals.userId = req.session.userId;
     res.locals.username = req.session.username;
     res.locals.isAutorize = true;
     res.locals.role = req.session.role;
