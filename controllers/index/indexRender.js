c789bf8d8462eb77847ef035d172ee379561e238
Dmitry Rum <acopalypse@yandex.ru>
2021-07-30 13:23:16 +0300

    styles fix

--- controllers/index/indexRender.js
+++ controllers/index/indexRender.js
@@ -1,11 +1,11 @@
 const { Products } = require('../../models/init');
 
 const indexRender = (req, res) => {
+  // Главная страница
   Products.findAll({ where: { status: 'new' }, raw: true }).then((db) => {
     console.log(db);
     res.render('indexPage', { db });
   });
-  // Главная страница
 };
 
 module.exports = indexRender;
